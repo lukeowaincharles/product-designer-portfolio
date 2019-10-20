1902ba37bf2a7495b7fe46046d413a3edba7db68
Luke Charles <lukeowaincharles@gmail.com>
2019-10-20 07:39:38 +0100

    Added styles to hero and moved hero class container into component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,15 +4,26 @@ import { Link } from 'react-scroll';
 export default class Hero extends React.Component {
 
   render() {
+
+    const styles = {
+      hero: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }
+    }
     return (
-      <div className="container">
-        <div className="hero__wrapper">
-          <div className="hero__content">
-            <p className="sub-title">Portfolio</p>
-            <h1>Powered by problems, purpose and people.</h1>
-            <p>I'm Liz, a London based Senior Digital Product Designer at Inktrap.</p>
+      <div className="hero" style={styles.hero}>
+        <div className="container">
+          <div className="hero__wrapper">
+            <div className="hero__content">
+              <p className="sub-title">Portfolio</p>
+              <h1>Powered by problems, purpose and people.</h1>
+              <p>I'm Liz, a London based Senior Digital Product Designer at Inktrap.</p>
+            </div>
+            <Link className="button-scroll" to="#whatido" spy={true} smooth={true} duration={800} aria-label="Scroll to what I do"></Link>
           </div>
-          <Link to="#whatido" spy={true} smooth={true} duration={800} aria-label="Scroll to what I do"></Link>
         </div>
       </div>
     )
