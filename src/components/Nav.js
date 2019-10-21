7ef73d1066321a44b6ee1c4ab8858ff250050199
Luke Charles <lukeowaincharles@gmail.com>
2019-10-21 21:20:00 +0100

    Added menu styles and functionality - Ideally need to break into 2 components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,6 +7,7 @@ export default class Nav extends React.Component {
     super(props);
     this.state={
       menuOpen: false,
+      open: this.props.open ? this.props.open:false,
     }
   }
 
@@ -16,6 +17,10 @@ export default class Nav extends React.Component {
 
   handleClick(){
     this.setState({open:!this.state.open});
+  }
+
+  handleLinkClick() {
+    this.setState({menuOpen: false});
   }
 
   render() {
@@ -32,6 +37,7 @@ export default class Nav extends React.Component {
         padding: '4px',
         position: 'fixed',
         marginTop: '31px',
+        zIndex: '6',
       },
       burger: {
         width: '24px',
@@ -54,15 +60,68 @@ export default class Nav extends React.Component {
         backgroundColor: this.state.open ? '#ffffff': '#FF2C9E',
         transformOrigin: 'top left',
         marginTop: '5px',
+      },
+      menuOverlay: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100%',
+        width: '100vw',
+        transform: this.state.open ? 'translateX(0)' : 'translateX(-100%)',
+        display: 'flex',
+        flexDirection: 'column',
+        background: '#FF2C9E',
+        color: 'white',
+        transition: 'transform 500ms ease-in-out',
+        zIndex: 5,
+      },
+      menuItems: {
+        marginTop: '80px',
+        width: '100%',
       }
     }
+
+    const menu = [
+      {id: 1, link: '#home', name: 'Home'},
+      {id: 2, link: '#whatIDo', name: 'What I do'},
+      {id: 3, link: '#howIDoIt', name: 'How I do it'},
+      {id: 4, link: '#whoIDoItFor', name: 'Who I do it for'},
+      {id: 5, link: '#Testimonials', name: 'Testimonials'},
+      {id: 6, link: '#whoIAm', name: 'Who I am'},
+      {id: 7, link: '#getInTouch', name: 'Get in touch'},
+    ];
+
+    const menuItems = menu.map((item, index) => {
+      return (
+        <h2 key={index}>
+          <Link
+            to={item.link}
+            onClick={()=>{this.handleLinkClick();}}
+          >
+            {item.name}
+          </Link>
+        </h2>
+      )
+    });
+
     return (
-      <div className="container">
-        <div style={styles.ham} className="hamburger" onClick={this.props.onClick ? this.props.onClick: () => {this.handleClick();}}>
-          <span style={{...styles.burger,...styles.burgerTop }}></span>
-          <span style={{...styles.burger,...styles.burgerMiddle }}></span>
-          <span style={{...styles.burger,...styles.burgerBottom }}></span>
+      <div>
+        <div className="container">
+          <div style={styles.ham} className="hamburger" onClick={this.props.onClick ? this.props.onClick: () => {this.handleClick();}}>
+            <span style={{...styles.burger,...styles.burgerTop }}></span>
+            <span style={{...styles.burger,...styles.burgerMiddle }}></span>
+            <span style={{...styles.burger,...styles.burgerBottom }}></span>
+          </div>
         </div>
+        <div style={styles.menuOverlay}>
+          {this.state.open ?
+          <div className="container">
+            <div style={styles.menuItems} open={this.state.menuOpen}>
+              {menuItems}
+            </div> 
+          </div> : null
+          }
+        </div>  
       </div>
     )
   }
