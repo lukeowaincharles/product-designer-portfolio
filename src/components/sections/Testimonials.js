76d4f8b7cba2c7f22625e999274a4fd2678cf696
Luke Charles <lukeowaincharles@gmail.com>
2019-11-03 17:14:42 +0000

    Small refactor of testimonials.js, added assets for the component

--- src/components/sections/Testimonials.js
+++ src/components/sections/Testimonials.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Slider from 'react-slick';
+import Quote from '../../assets/images/quote-marks.svg';
+import Jim from '../../assets/images/jim.png';
+import Mazz from '../../assets/images/mazz.png';
+import Michael from '../../assets/images/michael.png';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -43,42 +47,63 @@ export default class Testimonials extends React.Component {
           <Slider {...settings}>
             <div>
               <div style={styles.sliderContainer}>
-                <img alt="quote-marks" src="" />
+                <div className="quote-wrapper">
+                  <img alt="quote-marks" src={Quote} />
+                </div>
                 <p>Liz is an exceptional designer and someone whom I learnt a lot from. She has the ability to breakdown problems and project briefs in complex industries such as Fintech and form effective ideas accordingly. Her work is always underpinned by a clear and intuitive process and invaluable experience.</p>
+                <hr />
 
                 <div className="author__wrapper">
-                  <div className="author">
-                    <img alt="" src="" />
-                    <p>Jim Silvy</p>
-                    <p>Freelance Digital Designer</p>
+                  <div className="author__content">
+                    <div className="author__image">
+                      <img alt="Jim Silvy" src={Jim} />
+                    </div>
+                    <div className="author__description">
+                      <p className="coustard">Jim Silvy</p>
+                      <p>Freelance Digital Designer</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
             <div>
             <div style={styles.sliderContainer}>
-              <img alt="quote-marks" src="" />
+              <div className="quote-wrapper">
+                <img alt="quote-marks" src={Quote} />
+              </div>
               <p>I worked with Liz on a very challenging Wealth project during my time at Barclays. She always acted professionally and was able to produce high quality designs even with constantly changing requirements. I would recommend working with Liz and I would happily do so again.</p>
+              <hr />
 
                 <div className="author__wrapper">
-                  <div className="author">
-                    <img alt="" src="" />
-                    <p>Michael Wallace</p>
-                    <p>Barclays Product Manager</p>
+                  <div className="author__content">
+                    <div className="author__image">
+                      <img alt="Michael Wallace" src={Michael} />
+                    </div>
+                    <div className="author__description">
+                      <p className="coustard">Michael Wallace</p>
+                      <p>Barclays Product Manager</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
             <div>
             <div style={styles.sliderContainer}>
-              <img alt="quote-marks" src="" />
+              <div className="quote-wrapper">
+                <img alt="quote-marks" src={Quote} />
+              </div>
               <p>Liz is extremely dedicated, passionate and takes great pride in her work. She is always full of new ideas and has a natural eye for detail when it comes to branding and web design. Working with Liz was an absolute pleasure, she's great fun to work alongside and I believe she would be an asset to any creative team.</p>
+              <hr />
 
                 <div className="author__wrapper">
-                  <div className="author">
-                    <img alt="" src="" />
-                    <p>Mazz Napier</p>
-                    <p>Eight Arms Project Manager</p>
+                  <div className="author__content">
+                    <div className="author__image">
+                      <img alt="Mazz Napier" src={Mazz} />
+                    </div>
+                    <div className="author__description">
+                      <p className="coustard">Mazz Napier</p>
+                      <p>Eight Arms Project Manager</p>
+                    </div>
                   </div>
                 </div>
               </div>
