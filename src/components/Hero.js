c438b5ee918b680e0c2393ae55f539e6ac43f26a
Luke Charles <lukeowaincharles@gmail.com>
2019-11-10 14:25:41 +0000

    Removed styles from React component, added tween etc - Needs some more work/thought about how the highlight works and sits

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,27 +1,53 @@
 import React from 'react';
 import { Link } from 'react-scroll';
+import { Controller, Scene } from 'react-scrollmagic';
+import { Tween } from 'react-gsap';
 
 export default class Hero extends React.Component {
 
   render() {
 
-    const styles = {
-      hero: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'relative',
-        height: '100vh',
-        backgroundColor: '#FFEBF0',
-      }
-    }
     return (
-      <div className="hero" style={styles.hero} name="#home">
+      <div className="hero" name="#home">
         <div className="container">
           <div className="hero__wrapper">
             <div className="hero__content">
               <p className="sub-title">Portfolio</p>
               <h1>Powered by problems, purpose and people.</h1>
+              <Controller>
+                <Scene duration={300}
+                  pin={false}
+                  reverse={false}
+                  offset={50}
+                  indicators={true}
+                  triggerElement="#svgHighlightHero"
+                >
+                  {(progress, event) => {
+                    return (
+                      <Tween 
+                        from={{
+                          css: {
+                            width: '0',
+                          },
+                          ease: 'Circ.easeOutExpo',
+                        }}
+                        to={{
+                          css: {
+                            width: '100%',
+                          },
+                          ease: 'Circ.easeOutExpo',
+                        }}
+                        totalProgress={progress}
+                        paused
+                        playState={
+                          (event.type === 'enter' && event.scrollDirection === 'FORWARD') ? 'play' :  null }
+                      >
+                        <span className="highlight highlight--long" id="svgHighlightHero"></span>
+                      </Tween>
+                    );
+                  }}
+                </Scene>
+              </Controller>
               <p className="text-size-20">I'm Liz, a London based Senior Digital Product Designer at Inktrap.</p>
             </div>
             <Link className="button-scroll" to="#whatIDo" spy={true} smooth={true} duration={800} aria-label="Scroll to what I do"></Link>
