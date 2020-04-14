aa1854fc1b540baa4aa8aba6e91b865db5a05aba
Luke Charles <lukeowaincharles@gmail.com>
2020-04-14 14:07:47 +0100

    removed png for gps and uploaded an svg

--- src/components/sections/RecentProjects.js
+++ src/components/sections/RecentProjects.js
@@ -2,7 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import GPSIMAGE from "../../assets/images/gps-project.png";
+import GPSIMAGE from "../../assets/images/gps-project.svg";
 import HUMNIMAGE from "../../assets/images/humn-project.svg";
 import TRXIMAGE from "../../assets/images/trx-project.svg";
 
